ee1fb859937b7d1edc1b3447167812ae1f18f5b7
usamahbass <basalamahusamah7@gmail.com>
2021-01-08 22:53:24 +0700

    setup: @chakra-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import theme from "./theme";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <link rel="shortcut icon" href="/bass.jpg" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/theme/index.js
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: `'Open Sans', sans-serif`,
+  },
+});
+
+export default theme;
